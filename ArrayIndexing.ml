aec854024c70a5a2831088589dd42d242142399e
Aditya Shibu <as2397@hw.ac.uk>
2023-12-18 22:03:37 +0400

    Reverted "Added basic functions for Array Indexing"

--- ArrayIndexing.ml
+++ ArrayIndexing.ml
@@ -1,10 +1,8 @@
-type 'a place = 
-  | Place of 'a list * 'a list
-  | List of 'a list;;
+type 'a place = 'a list * 'a list;;
 
 exception IndexOutofBounds
 
-let getPlace (List (xs)) n =
+let getPlace xs n : 'a place =
   let rec splitAtIndex n acc lst = 
     match n, lst with 
     | 0, _ -> (List.rev acc, lst)
@@ -14,33 +12,31 @@ let getPlace (List (xs)) n =
     if n < 0 || n > List.length xs then 
       raise IndexOutofBounds
     else 
-      let left, right = splitAtIndex n [] xs in 
-      Place (left, right);;
+      splitAtIndex n [] xs;;
 
-let collapse (Place (before, after)) = 
-  List (List.append before after)
+let collapse ((before, after) : 'a place) = 
+  List.append before after;;
 
-let isStart (Place (before, _) : 'a place) = 
+let isStart ((before, _) : 'a place) = 
   List.length before == 0;;
 
-let isEnd (Place (_, after) : 'a place) = 
+let isEnd ((_, after) : 'a place) = 
   List.length after == 0;;
 
-let next (Place (before, after) : 'a place) = 
+let next ((before, after) : 'a place) = 
   match List.rev before with 
   | [] -> raise IndexOutofBounds 
-  | h :: t -> Place (List.rev t, h :: after);;
+  | h :: t -> (List.rev t, h :: after);;
 
-let lookup (Place (_, after) : 'a place) = 
+let lookup ((_, after) : 'a place) = 
   match after with 
   | [] -> raise IndexOutofBounds
   | h :: t -> h;;
 
-let delete (Place (before, after) : 'a place) = 
+let delete ((before, after) : 'a place) = 
   match after with 
   | [] -> raise IndexOutofBounds 
-  | _ :: t -> (before, t);;
+  | _ :: t -> (before, t)
 
-let insert x (Place (before, after) : 'a place) = 
-  (List.append before [x], after);;
-
+let insert x ((before, after) : 'a place) = 
+  (List.append before [x], after)
