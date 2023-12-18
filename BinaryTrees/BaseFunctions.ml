550a40a0fe1d24e32d2e8be1fa4b374d79cf113b
Aditya Shibu <as2397@hw.ac.uk>
2023-12-19 01:05:17 +0400

    Converted BaseFunctions into a module

--- BinaryTrees/BaseFunctions.ml
+++ BinaryTrees/BaseFunctions.ml
@@ -1,50 +1,56 @@
-type 'a tree = 
-  | Lf
-  | Br of 'a * 'a tree * 'a tree;;
+module BinarySearchTree : sig
+  type 'a tree =
+    | Lf
+    | Br of 'a * 'a tree * 'a tree
 
-(* Function to calculate the total number of elements in the BST *)
-let rec size t = 
-  match t with 
-  | Lf -> 0 
-  | Br (ele, br1, br2) -> 1 + size br1 + size br2;;
+  val size : 'a tree -> int
+  val depth : 'a tree -> int
+  val dft : 'a tree -> 'a list
+  val flatten : 'a tree -> 'a list
+  val member : 'a -> 'a tree -> bool
+  val insert : 'a -> 'a tree -> 'a tree
+  val toTree : 'a list -> 'a tree
+end = struct
+  type 'a tree =
+    | Lf
+    | Br of 'a * 'a tree * 'a tree
 
-(* Function calculate the height of the given BST *)
-let rec depth t = 
-  match t with 
-  | Lf -> 0 
-  | Br (ele, br1, br2) -> 
-    1 + max (depth br1) (depth br2);;
+  let rec size t =
+    match t with
+    | Lf -> 0
+    | Br (_, br1, br2) -> 1 + size br1 + size br2
 
-(* Function for Depth first traversal in a BST *)
-let rec dft t = 
-  match t with 
-  | Lf -> []
-  | Br (ele, br1, br2) -> ele :: List.append (dft br1) (dft br2);;
+  let rec depth t =
+    match t with
+    | Lf -> 0
+    | Br (_, br1, br2) -> 1 + max (depth br1) (depth br2)
 
-(* Flatten out the BST with the root element in between *)
-let rec flatten t = 
-  match t with 
-  | Lf -> [] 
-  | Br (ele, br1, br2) -> List.append (flatten br1) (ele :: flatten br2);;
+  let rec dft t =
+    match t with
+    | Lf -> []
+    | Br (ele, br1, br2) -> ele :: List.append (dft br1) (dft br2)
 
-(* Function to check if a given element is part of the given BST *)
-let rec member x t = 
-  match t with 
-  | Lf -> false 
-  | Br (ele, br1, br2) -> 
-    if x == ele then true 
-    else if x < ele then member x br1 else member x br2;;
+  let rec flatten t =
+    match t with
+    | Lf -> []
+    | Br (ele, br1, br2) -> List.append (flatten br1) (ele :: flatten br2)
 
-(* Function to insert an element into a BST *)
-let rec insert x t = 
-  match t with 
-  | Lf -> Br (x, Lf, Lf)
-  | Br (ele, br1, br2) -> 
-    if x <= ele then Br (ele, insert x br1, br2)
-    else Br (ele, br1, insert x br2);;
+  let rec member x t =
+    match t with
+    | Lf -> false
+    | Br (ele, br1, br2) ->
+      if x = ele then true
+      else if x < ele then member x br1 else member x br2
 
-(* Function to convert a given list to a BST *)
-let rec toTree lst = 
-  match lst with 
-  | [] -> Lf 
-  | x :: xs -> insert x (toTree xs);;
+  let rec insert x t =
+    match t with
+    | Lf -> Br (x, Lf, Lf)
+    | Br (ele, br1, br2) ->
+      if x <= ele then Br (ele, insert x br1, br2)
+      else Br (ele, br1, insert x br2)
+
+  let rec toTree lst =
+    match lst with
+    | [] -> Lf
+    | x :: xs -> insert x (toTree xs)
+end
